08f214e65d441fd7484c5c900da2bd2ecb6ebb1a
Md. Ariful Islam <arifjmamun24@gmail.com>
2023-03-28 01:11:47 +0600

    fix database migration

--- src/TodoCQRS.API/Program.cs
+++ src/TodoCQRS.API/Program.cs
@@ -9,14 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 MappingConfig.Configure();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        x => x.MigrationsAssembly("TodoCQRS.Infrastructure")));
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(TodosController).Assembly));
 builder.Services.AddScoped<ITodoRepository, TodoRepository>();
 
-builder.Services.Configure<RouteOptions>(options =>
-{
-    options.LowercaseUrls = true;
-});
+builder.Services.Configure<RouteOptions>(options => { options.LowercaseUrls = true; });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
